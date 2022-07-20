eb237436fb2f14a9cca165898edb110b6a92661a
aalissy <32420245+aalissy@users.noreply.github.com>
2022-07-19 19:39:55 -0500

    Added movie genres based on id from Movie Database API

--- script.js
+++ script.js
@@ -4,3 +4,82 @@ var nytUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query="
 var tmdbKey = "api_key=9057cf76a0f4698dd3c5d50c15b617fc"
 
 var tmdbUrl = "https://api.themoviedb.org/3/movie/550?query="
+
+var genres = [
+    {
+        "id": 12,
+        "genre": "Adventure"
+    },
+    {
+        "id": 14,
+        "genre": "Fantasy"
+    },
+    {
+        "id": 16,
+        "genre": "Animation"
+    },
+    {
+        "id": 27,
+        "genre": "Horror"
+    },
+    {
+        "id": 18,
+        "genre": "Drama"
+    },
+    {
+        "id": 28,
+        "genre": "Action"
+    },
+    {
+        "id": 35,
+        "genre": "Comedy"
+    },
+    {
+        "id": 36,
+        "genre": "History"
+    },
+    {
+        "id": 37,
+        "genre": "Western"
+    },
+    {
+        "id": 53,
+        "genre": "Thriller"
+    },
+    {
+        "id": 80,
+        "genre": "Crime"
+    },
+    {
+        "id": 878,
+        "genre": "Science Fiction"
+    },
+    {
+        "id": 99,
+        "genre": "Documentary"
+    },
+    {
+        "id": 9648,
+        "genre": "Mystery"
+    },
+    {
+        "id": 10402,
+        "genre": "Music"
+    },
+    {
+        "id": 10749,
+        "genre": "Romance"
+    },
+    {
+        "id": 10751,
+        "genre": "Family"
+    },
+    {
+        "id": 10752,
+        "genre": "War"
+    },
+    {
+        "id": 10770,
+        "genre": "TV Movie"
+    }
+]
