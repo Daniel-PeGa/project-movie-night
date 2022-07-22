403e2cf84712835b437436fce417706fb0752ba0
aalissy <32420245+aalissy@users.noreply.github.com>
2022-07-21 20:06:31 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //New York Times movie review info
-var nytKey = "&api-key=MaQgrhzm0bWhoTQZXafGplCBCmjaWwKI";
-var nytUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=";
+//var nytKey = "&api-key=MaQgrhzm0bWhoTQZXafGplCBCmjaWwKI";
+//var nytUrl = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=";
 //the Movie data base info
 var tmdbKey = "api_key=9057cf76a0f4698dd3c5d50c15b617fc";
 var tmdbUrl = "https://api.themoviedb.org/3/movie/550?query=";
+//TODO: add key and URL for movie quotes
 //either this
 var btn1 = $("#mood1");
 var btn2 = $("#mood2");
@@ -14,6 +15,15 @@ var btn6 = $("#mood6");
 //or this not both
 var btnForm = $(".btn");
 var userinput = btnForm.textContent;
+
+//TODO: When random button gets pressed generates random movie id
+//TODO: From random id fetch into the API
+//TODO: Make a variable for ID into localstorage and pull it out
+//TODO: Pull ID number from Dune and put it into TMDB API
+//TODO: Display movie info from TMDB API
+function movieQuote(){
+    fetch();
+}
 
 //TODO: Base framework for api search on TMDB
 function randomMovie(){
@@ -28,6 +38,6 @@ function showRandomMovie() {
 
 //TODO: Base framework for api search on TMDB 
 //!will need to be based on movie picked from TMDB
-function movieReview(){
-    fetch(nytKey + userInputTwo + nytUrl);
-}
+//function movieQuote(){
+    //fetch(nytKey + userInputTwo + nytUrl);
+//}
